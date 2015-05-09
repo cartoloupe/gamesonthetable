1bb69ba6ff91f3749ff50a5a5a74e60a21954e61
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-05-09 15:17:53 -0400

    working websockets with help from Ryan Norman

--- app/assets/javascripts/moves.js
+++ app/assets/javascripts/moves.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
 
   var dispatcher = new WebSocketRails(window.location.host + '/websocket');
   dispatcher.subscribe("create");
-  dispatcher.bind('create', function () {
-    return 
+  dispatcher.bind('create', function() {
+    return
   })
 
   $('#submit-move').on('click', function() {
-    dispatcher.trigger("create", {
-      moves: 3
-    });
+    submitMove($('#new-move').val());
   });
 });
 
 
-// function submitMove () {
-//   console.log("Clicked");
-// }
+function submitMove(value) {
+  $.post("/moves", {
+    move: {number_of_moves: value}
+  });
+}
 
 
 function movesList(message) {
