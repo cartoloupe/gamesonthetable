1d607a418bcd7d9d51d08d55cac4c8d1450642ab
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-05-31 16:58:23 -0400

    add dispatcher to user.js with trigger and bind to users logging in

--- app/assets/javascripts/angular/controllers/user.js
+++ app/assets/javascripts/angular/controllers/user.js
@@ -4,36 +4,39 @@ movesApp.config(function(AuthProvider) { // Configure Auth service with AuthProv
 });
 
 
-movesApp.controller('UserController', ['$scope', 'Auth', function($scope, Auth) {
+movesApp.controller('UserController', ['$scope', 'Auth', 'dispatcher', function($scope, Auth, dispatcher) {
 
-  $scope.current_user = 'no user retrieved yet';
+  $scope.current_user = '...';
   $scope.current_users = [];
 
   $scope.getUser = function() {
-    console.log("getting user");
+    //console.log("getting user");
 
     Auth.currentUser().then(function(user) {
       // User was logged in, or Devise returned
       // previously authenticated session.
-      console.log(user.email); // => {id: 1, ect: '...'}
+      //console.log(user.email); // => {id: 1, ect: '...'}
       $scope.current_user = user.email;
 
       // only add user to current_users if they don't already exist
       if ($scope.current_users.indexOf(user.email) == -1){
         $scope.current_users.push(user.email);
       }
-      console.log([$scope.current_users, user.email]);
+
+      dispatcher.trigger('user.logged_in', {user: user});
     }, function(error) {
       // unauthenticated error
       console.log("unauthenticated")
     });
   }
 
-  /*
-  $scope.getUsers = function() {
-    //
-    $scope.current_users;
-  }
-  */
+  dispatcher.bind('user', 'logged_in', function(user) {
+    console.log("user logged_in: " + user.email);
+
+    // only add user to current_users if they don't already exist
+    if ($scope.current_users.indexOf(user.email) == -1){
+      $scope.current_users.push(user.email);
+    }
+  });
 
 }]);
