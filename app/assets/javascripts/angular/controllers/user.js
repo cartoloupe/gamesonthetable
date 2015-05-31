61b591969cab8b452459a23d163f3e19748198ad
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-05-31 10:52:21 -0400

    WIP

--- app/assets/javascripts/angular/controllers/user.js
+++ app/assets/javascripts/angular/controllers/user.js
@@ -1,35 +1,30 @@
 // var myApp = angular.module('games', ['Devise']);
 
-movesApp.config(function(AuthProvider) { // Configure Auth service with AuthProvider 
+movesApp.config(function(AuthProvider) { // Configure Auth service with AuthProvider
 });
 
 
 movesApp.controller('UserController', ['$scope', 'Auth', function($scope, Auth) {
 
-  $scope.greeting = 'no user retrieved yet';
-
-  // Auth.currentUser().then(function(user) { 
-  // 	// User was logged in, or Devise returned 
-  // 	// previously authenticated session. 
-  // 	console.log(user); // => {id: 1, ect: '...'} 
-  // 	}, function(error) { 
-  // 	// unauthenticated error
-  // 	console.log("unauthenticated")
-  // });
-
+  $scope.current_user = 'no user retrieved yet';
+  $scope.current_users = 'none yet';
 
   $scope.getUser = function() {
-  	console.log("getting user");
+    console.log("getting user");
 
-	  Auth.currentUser().then(function(user) { 
-	  	// User was logged in, or Devise returned 
-	  	// previously authenticated session. 
-	  	console.log(user.email); // => {id: 1, ect: '...'}
-	  	$scope.greeting = user.email;
-	  	}, function(error) { 
-	  	// unauthenticated error
-	  	console.log("unauthenticated")
-	  });  	
+    Auth.currentUser().then(function(user) {
+      // User was logged in, or Devise returned
+      // previously authenticated session.
+      console.log(user.email); // => {id: 1, ect: '...'}
+      $scope.current_user = user.email;
+    }, function(error) {
+      // unauthenticated error
+      console.log("unauthenticated")
+    });
+  }
+
+  $scope.getUsers = function() {
+    //
   }
 
 }]);
