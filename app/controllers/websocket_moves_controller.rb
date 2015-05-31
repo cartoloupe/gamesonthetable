1653c7d83bd0d0ca6a6b51e8ecf7e230da99f5d9
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-05-31 17:05:17 -0400

    add destroy and user_logged_in methods; user_logged_in should probably belong in another controller

--- app/controllers/websocket_moves_controller.rb
+++ app/controllers/websocket_moves_controller.rb
@@ -1,7 +1,15 @@
 class WebsocketMovesController < WebsocketRails::BaseController
+  #def create
+  #  raise message.inspect
+  #end
 
-  def create
-    raise message.inspect
+  def destroy
+    Move.first.destroy
+    render :nothing
+  end
+
+  def user_logged_in
+    WebsocketRails[:user].trigger 'logged_in', message[:user]
   end
 
 end
