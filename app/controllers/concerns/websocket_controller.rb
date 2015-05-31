d24969b991429d9aaebadde025f6a22b6b7091a6
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-05-31 17:03:18 -0400

    looks like this controller is not being used

--- app/controllers/concerns/websocket_controller.rb
+++ app/controllers/concerns/websocket_controller.rb
@@ -1,8 +1,8 @@
 class WebsocketController < WebsocketRails::BaseController
 
-  def show
-    Move.all
-    broadcast_message :create, { new_moves: 4}
-  end
+  #def show
+  #  Move.all
+  #  broadcast_message :create, { new_moves: 4}
+  #end
 
 end
